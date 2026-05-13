e729562ddd156fa4de043c49f71156f7b60a3e56
VJ-108 <vaibhav160205@gmail.com>
2026-05-13 21:41:04 +0530

    .

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://13.48.195.232:3000/api";
-export const SERVER_URL = "http://13.48.195.232:3000";
+export const API_BASE_URL = "http://13.51.166.69:3000/api";
+export const SERVER_URL = "http://13.51.166.69:3000";
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,6 @@ import path from "path";
 
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	server: {
-		host: "0.0.0.0",
-		port: 5173,
-		strictPort: true,
-	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "src"),
