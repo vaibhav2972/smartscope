01b668a2bba4cdb8e7e51f54b402bca244694822
VJ-108 <vaibhav160205@gmail.com>
2026-05-14 08:41:41 +0530

    .

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://13.60.19.244:3000/api";
-export const SERVER_URL = "http://13.60.19.244:3000";
+export const API_BASE_URL = "http://13.60.231.184:3000/api";
+export const SERVER_URL = "http://13.60.231.184:3000";
